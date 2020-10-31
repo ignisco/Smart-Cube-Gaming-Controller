077097bdc2167080b28338e94d6ee9ef7ec2fcfb
Christian Templen Grave <christgrave@gmail.com>
2020-10-31 17:16:27 +0100

    Made all paths relative

--- Bluetooth LE Web Scraper.py
+++ Bluetooth LE Web Scraper.py
@@ -6,18 +6,20 @@ Created on Tue Oct 27 01:55:49 2020
 """
 
 
+import os
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities 
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from pynput.keyboard import Key, Controller
 
-keyboard = Controller()   
+keyboard = Controller()
 
 capabilities = DesiredCapabilities.CHROME
 capabilities['goog:loggingPrefs'] = { 'browser':'ALL' }
 
 driver = webdriver.Chrome(desired_capabilities=capabilities, executable_path=r'chromedriver.exe')
 
-driver.get('file:///C:/Users/Christian/github/GoCube/index.html')
+html_location = os.path.dirname(os.path.abspath(__file__)) + "/index.html"
+driver.get(html_location)
 
 dic = {"U": "H",
        "U'": "E",
@@ -35,6 +37,6 @@ while True:
             entry = str(entry).split('"')[1][7:]
             entry = entry.replace("\\", "")
             keyboard.press(dic[entry])
-            
+
         except:
             pass
